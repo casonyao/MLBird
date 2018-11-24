05e7aba4c5a8f21365a14b8f4760bf5b4f9f9aec
casonyao <472473554@qq.com>
2018-11-24 22:40:09 +0800

    Create tf.Variable.py

---
+++ tf.Variable.py
@@ -0,0 +1,11 @@
+import tensorflow as tf 
+
+weights = tf.Variable(tf.random_normal([784,200],stddev=0.35),name="weights")
+biases = tf.Variable(tf.zeros([200]),name="biases")
+
+init_op = tf.initialize_all_variables()
+
+with tf.Session() as sess:
+	sess.run(init_op)
+	print(weights)
+	print(biases)
